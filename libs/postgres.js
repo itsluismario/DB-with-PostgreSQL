32e435375f4f70af15aba765bb889310a404e3f9
itsluismario <itsluismario@gmail.com>
2023-07-13 20:20:14 -0600

    :construction:  Switching to mysql It is possible to switch from postgresql to mysql

--- libs/postgres.js
+++ libs/postgres.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 async function getConnection() {
   const client = new Client({
     host: 'localhost',
-    port: 2001,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGNAME,
+    port: 3307,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
   await client.connect();
   return client;
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -8,15 +8,22 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 
 // Constructing the connection URI using the encoded user, password, host, port, and database name
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: false
+  dialect: 'mysql',
+  logging: true,
+
+  pool: {
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle
+  }
 });
 
-setupModels(sequelize);
+setupModels(sequelize)
 
-sequelize.sync();
+sequelize.sync()
 
 module.exports = sequelize;
