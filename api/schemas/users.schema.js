83875401672d4bd8448d10b93b258f06684b5900
itsluismario <itsluismario@gmail.com>
2023-07-22 10:29:39 -0600

    :sparkles: Modifying an entity
    :

--- api/schemas/users.schema.js
+++ api/schemas/users.schema.js
@@ -9,22 +9,22 @@ const role = Joi.string().min(5);
 const createUserSchema = Joi.object({
   name: name.required(),
   email: email.required(),
-  password: password.required()
-  // role: role.required()
+  password: password.required(),
+  role: role.required()
 });
 
 const parcialUpdateUserSchema = Joi.object({
   name: name,
   email: email,
   password: password,
-  // role:role
+  role:role
 });
 
 const updateUserSchema = Joi.object({
   name: name.required(),
   email: email.required(),
   password: password.required(),
-  // role: role.required()
+  role: role.required()
 });
 
 const getUserSchema = Joi.object({
